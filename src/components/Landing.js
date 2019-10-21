59b65cc9b3bce5b055bb7d6ce72abffe1e0aac98
AudreyHal <chiomahalim@gmail.com>
2019-10-21 04:48:58 -0700

    Initial Commit

---
+++ src/components/Landing.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Landing extends Component{
+	constructor(){
+		super();
+		this.state={
+				}
+}
+	
+	render(){
+		return (		
+			<div className="Landing">
+				hhhh		
+		    </div>	
+		)
+	}
+
+}
+
+export default Landing;
